e14a90e5d02adf0d0f7cfe48489b53ccfffea06a
vman <vardhaman.rd@gmail.com>
2018-06-17 23:49:46 +0100

    formatting

--- Office365.SiteDesigns.Deployment/Utilities/ApplySiteDesign.ps1
+++ Office365.SiteDesigns.Deployment/Utilities/ApplySiteDesign.ps1
@@ -1,8 +1,4 @@
 
 $siteDesignToApply = Get-SPOSiteDesign | Where-Object {$_.Title -eq "CnC Basic Communication Site"}
 
-Invoke-SPOSiteDesign -Identity $siteDesignToApply.ID -WebUrl "https://vrdmn.sharepoint.com/sites/modernsite"
-
-
-
-
+Invoke-SPOSiteDesign -Identity $siteDesignToApply.ID -WebUrl "https://vrdmn.sharepoint.com/sites/BasicSite"
